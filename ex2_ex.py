dbc6a74d21eeca8632a3c956cc52a90534356d4d
rikutoyamada777 <rikuto.yamada@nds-osk.co.jp>
2025-05-16 09:51:20 +0900

    2章演習問題

---
+++ ex2_ex.py
@@ -0,0 +1,17 @@
+#(1)
+a = 3
+b= 4
+
+#(2)
+print(a*b)
+
+#(3)
+print(a,"*",b,"=12")
+
+#(4)
+moziretu = "value = 12345"
+print(int(moziretu[8:]))
+
+#(5)
+dog = "I like dog"
+print(dog.replace("dog", "cat"))
